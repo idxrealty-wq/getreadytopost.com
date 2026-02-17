c760e55a9b831d69484988625084609a4c745904
Christopher Sampson <idxrealty@gmail.com>
2026-02-17 00:28:41 -0500

    Fix Rate My Listing page background layout

--- src/app/rate-my-listing/page.tsx
+++ src/app/rate-my-listing/page.tsx
@@ -3,19 +3,19 @@ import RateMyListing from "@/components/RateMyListing";
 
 export default function RateMyListingPage() {
   return (
-    <main className="pt-20 min-h-screen relative">
-      <div className="absolute inset-0 z-0">
+    <div className="relative min-h-screen">
+      <div className="fixed inset-0 z-0">
         <img
           src="https://us.chat-img.sintra.ai/f3b53c23-1962-4de9-bee1-1ab563b224f9/1c6b6e83-767a-4a5f-9cc4-ea33a9ca148a/image.png?w=1200&h=896"
           alt="Background"
           className="w-full h-full object-cover"
         />
-        <div className="absolute inset-0 bg-[#1a2b4a]/85"></div>
+        <div className="absolute inset-0 bg-[#1a2b4a]/80"></div>
       </div>
       
-      <div className="relative z-10">
+      <div className="relative z-10 pt-20">
         <RateMyListing />
       </div>
-    </main>
+    </div>
   );
 }
