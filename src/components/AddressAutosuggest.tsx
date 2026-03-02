a6b10634eee0a1431bc30dbc0b785965d8be3911
Commet <commet@sintra.ai>
2026-03-02 14:47:52 -0500

    fix duplicate owner_name in interface

--- src/components/AddressAutosuggest.tsx
+++ src/components/AddressAutosuggest.tsx
@@ -22,7 +22,6 @@ interface ParcelResult {
   acres: string;
   taxable_value: string;
   assessed_value: string;legal_description: string;
-  owner_name: string;
   property_link: string;
   land_value: string;
   building_value: string;
